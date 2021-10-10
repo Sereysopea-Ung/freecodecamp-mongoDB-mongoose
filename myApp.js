8ebd67abb78507cc500b116058f7c55efb56140b
unknown <ungsereysopeacontact@gmail.com>
2021-10-10 09:51:04 +0700

    Done

--- myApp.js
+++ myApp.js
@@ -48,39 +48,65 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food},function(err,data){
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
+
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, function(err,data){
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId, function(err,person){
+    if (err) return console.log(err);
+    person.favoriteFoods.push(foodToAdd);
+    person.save(function(err,updatedPerson){
+      if (err) return console.log(err);
+      done(null, updatedPerson);
+    });
+  });
 };
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
-
-  done(null /*, data*/);
+  Person.findOneAndUpdate({name:personName},{age:ageToSet},{new:true},function(err,updatedDoc){
+    if (err) return console.log(err);
+    done(null,updatedDoc);
+  });
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId,function(err,removedDoc){
+    if (err) return console.log(err);
+    done(null,removedDoc);
+  });
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({name:nameToRemove},function(err,response){
+    if (err) return console.log(err);
+    done(null,response);
+  });
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find({favoriteFoods: foodToSearch})
+        .sort({name: 1})
+        .limit(2)
+        .select({age:0})
+        .exec(function(err,data){
+          if (err) return console.log(err);
+          done(null, data);
+        });
 };
 
 /** **Well Done !!**
